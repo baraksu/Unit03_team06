53714e2f594e8bf1a222eeca8285d5a5a920fa86
YotamOphir <yotam4ophir@gmail.com>
2025-12-12 10:29:30 +0200

    פונקציה 1

--- Unit03.java
+++ Unit03.java
@@ -1,7 +1,22 @@
 public class Unit03{
-    
+
+    // Yotam
     public static int shiftRight(int num)    {
+    if (num < 0 || num > 9999)
         return -1;
+    else {
+        int lastDigit = num % 10;
+        int newNum = num / 10;
+        if (num >= 0 && num < 10)
+            newNum = num;
+        else if (num > 9 && num < 100)
+            newNum = lastDigit*10 + newNum;
+        else if (num > 99 && num < 1000)
+            newNum = lastDigit*100 + newNum;
+        else if (num > 999 && num < 10000)
+            newNum = lastDigit*1000 + newNum;
+        return newNum; 
+        }
     }
     public static int shiftRight(int num, int k){
         return -1;
